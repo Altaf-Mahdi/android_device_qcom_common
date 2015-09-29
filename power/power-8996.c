edcc1cae76dcc640cecd9ce5b66deb2b50f43258
Tim Murray <timmurray@google.com>
2015-09-28 17:23:30 -0700

    power-8996: Modify screen-off behavior.
    Set sched_upmigrate to 99 and sched_downmigrate to 95 when the screen is
    off. This will make it harder to use the big cores when the screen is
    off (saving power), but will still allow them to be used in case there
    is actually a heavyweight task running.
    
    bug 24130678
    
    Change-Id: I8a8638f62e18cc8fac819d95c17e97ed4ff7e193

--- power/power-8996.c
+++ power/power-8996.c
@@ -380,7 +380,6 @@ int power_hint_override(__unused struct power_module *module,
 
 int set_interactive_override(__unused struct power_module *module, int on)
 {
-    return HINT_HANDLED; /* Don't excecute this code path, not in use */
     char governor[80];
 
     if (get_scaling_governor(governor, sizeof(governor)) == -1) {
@@ -393,7 +392,9 @@ int set_interactive_override(__unused struct power_module *module, int on)
         /* Display off */
         if ((strncmp(governor, INTERACTIVE_GOVERNOR, strlen(INTERACTIVE_GOVERNOR)) == 0) &&
             (strlen(governor) == strlen(INTERACTIVE_GOVERNOR))) {
-            int resource_values[] = {}; /* dummy node */
+            // sched upmigrate = 99, sched downmigrate = 95
+            // keep the big cores around, but make them very hard to use
+            int resource_values[] = {0x4E63, 0x4F5F};
             if (!display_hint_sent) {
                 perform_hint_action(DISPLAY_STATE_HINT_ID,
                 resource_values, ARRAY_SIZE(resource_values));
